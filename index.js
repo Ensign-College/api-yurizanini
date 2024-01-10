c0e9c9c9c5e2150c89f22365696d9faa9e30a84e
yurizanini <108382693+yurizanini@users.noreply.github.com>
2024-01-10 17:23:24 +0000

    Added express, and devcontainer

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+//import { Express } from "express";
+const express = require('express');//express makes APIs - connect frontend to database
+
+const app = express();//create an express application
+
+app.listen(3000);//listen for web requests from the frontend and don't stop
+
+const boxes = [
+    {
+        boxID: 1,
+    },
+    {
+        boxID: 2,
+    },
+    {
+        boxID: 3,
+    },
+    {
+        boxID: 4,
+    }
+
+];
+
+// 1 - URL
+// 2 - a function to return boxes
+// req - the reuest from the browser
+// res - the response to the browser
+app.get('/boxes', (req,res)=>{
+    //send boxes to the browser
+    res.send(JSON.stringify(boxes));//convert the boxes to a string
+});//return boxes to the user
+
 console.log("Hello");
